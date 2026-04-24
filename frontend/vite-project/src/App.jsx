f30c2a8d08e65885620af4c5b005b36bd10655c6
diyamnair <mnairdiya@gmail.com>
2026-04-24 19:23:02 +0530

    connect frontend to deployed backend

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   const handleSubmit = async () => {
     try {
-      const res = await fetch("http://localhost:5000/bfhl", {
+      const res = await fetch("https://graph-nest-fullstack.onrender.com/bfhl", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
